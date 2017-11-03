329e48e35bea87fc8c0a19ace6abd5f4211c1704
Liván Ramírez <livanr68@gmail.com>
2017-11-03 14:10:56 +0100

    Add project files.

---
+++ TestBranches/TestBranches.h
@@ -0,0 +1,32 @@
+
+// TestBranches.h : main header file for the PROJECT_NAME application
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'stdafx.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// main symbols
+
+
+// CTestBranchesApp:
+// See TestBranches.cpp for the implementation of this class
+//
+
+class CTestBranchesApp : public CWinApp
+{
+public:
+	CTestBranchesApp();
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+
+// Implementation
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CTestBranchesApp theApp;
